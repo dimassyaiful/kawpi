73a5dd214c3119c826a6e5dcc44fabdb88b903ae
DimPul <41774933+dimassyaiful@users.noreply.github.com>
2019-07-17 09:02:27 +0700

    Navbar change
    kalo mau ubah menu disini file nya
    
    view : template/menu

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -24,77 +24,23 @@
 
   
   <script src="<?= base_url(); ?>src/vendors/chart.js/Chart.min.js"></script>
+  <style type="text/css">
+    .navbar-nav a{
+      color: white !important;
+      margin-right: 10px;
+      margin-left: 10px;
+    }
+    .dropdown-menu a{
+      color: black !important;
+      margin-right: 10px;
+      margin-left: 10px;
+    }
+  </style>
 </head>
 
 <body>
-  <div class="container-scroller">
-    <!-- partial:partials/_horizontal-navbar.html -->
-    <div class="horizontal-menu">
-      <nav class="navbar top-navbar col-lg-12 col-12 p-0">
-        <div class="container">
-          <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-            <h2 style="color: white"> KAWPI </h2>
-          </div>
-          <div class="navbar-menu-wrapper d-flex align-items-center">
-           
-            <ul class="navbar-nav navbar-nav-right">
-              
-              <li class="nav-item nav-profile dropdown mr-0 mr-sm-2">
-                <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" >
-                  <img src="src/images/tony.jpg"/>
-                  <span class="nav-profile-name"><?= $this->session->userdata('nama'); ?></span>
-                </a>
-                <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
-                  <a class="dropdown-item">
-                    <i class="fa fa-user text-primary"></i>
-                    Profile
-                  </a>
-                  <a class="dropdown-item" href="<?= base_url(); ?>login/out"> 
-                    <i class="fa fa-sign-out text-primary"></i>
-                    Logout
-                  </a>
-                </div>
-              </li>
-            </ul>
-            <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="horizontal-menu-toggle">
-              <span class="mdi mdi-menu"></span>
-            </button>
-          </div>
-        </div>
-      </nav>
-      <nav class="bottom-navbar">
-        <div class="container">
-          <ul class="nav page-navigation">
-            <li class="nav-item">
-              <a class="nav-link" href="index.html">
-                <i class="fa fa-home menu-icon"></i>
-                <span class="menu-title">Dashboard</span>
-              </a>
-            </li>
-            
-            <li class="nav-item">
-              <a href="#" class="nav-link">
-                <i class="fa fa-vcard fa-fw menu-icon"></i>
-                <span class="menu-title">Data Anggota</span>
-              <div class="submenu">
-                <ul class="submenu-item">
-                  <li class="nav-item"><a class="nav-link" href="pages/forms/basic_elements.html">Pendaftar Baru</a></li>
-                  <li class="nav-item"><a class="nav-link" href="pages/forms/advanced_elements.html">Anggora Resmi</a></li>
-                </ul>
-              </div>
-            </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link">
-                <i class="fa fa-users menu-icon"></i>
-                <span class="menu-title">Data Pengguna</span>
-                </a>
-             
-            </li>
-           
-          </ul>
-        </div>
-      </nav>
-    </div>
+  
+  <?php $this->load->view('template/menu'); ?>
 
     <!-- partial -->
     <div class="container-fluid page-body-wrapper">
---
+++ application/views/template/menu.php
@@ -0,0 +1,41 @@
+<nav class="navbar navbar-expand-md navbar-dark" style="background-color: #00450f">
+    <a href="<?= base_url().'dashboard'; ?>" class="navbar-brand">
+        <h3> KAWPI</h3>
+    </a>
+    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav">
+            <a href="<?= base_url().'dashboard'; ?>" class="nav-item nav-link"><i class="fa fa-home"></i> Dashboard</a>
+            <?php 
+               $hak_akses = $this->session->userdata('hak_akses');
+               if($hak_akses == '1'){  //menu admin ?>
+                    <a href="<?= base_url().'pengguna'; ?>" class="nav-item nav-link"> <i class="fa fa-database"></i> Pengguna</a>
+                    <a href="<?= base_url().'anggota'; ?>" class="nav-item nav-link"> <i class="fa fa-database"></i> Anggota</a>
+                    <a href="<?= base_url().'pendaftar'; ?>" class="nav-item nav-link"> <i class="fa fa-database"></i> Pendaftar</a>
+               <?php }else if($hak_akses == '2'){ //menu ketua ?>
+
+               <?php }elseif($hak_akses == '3'){ //menu sekretaris ?>
+
+               <?php }elseif($hak_akses == '4'){ //menu anggota ?>
+
+               <?php }elseif($hak_akses == '5'){ //menu pendaftar ?>
+
+               <?php }else{
+                    $this->session->set_notif('Error, Hak akses tidak ditemukan','close','danger');
+                    redirect('login');
+                    exit;
+               }  ?>
+           
+        </div>
+        <div class="navbar-nav ml-auto nav-item dropdown" style="margin-right: 20px;">
+                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown"><?= $this->session->userdata('nama'); ?></a>
+                <div class="dropdown-menu">
+                    <a href="#" class="dropdown-item">Profile</a>
+                    <a class="dropdown-item" href="<?= base_url(); ?>login/out">Logout</a>
+                </div>
+            </div>
+    </div>
+  </nav>
